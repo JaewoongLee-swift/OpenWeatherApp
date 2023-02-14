a07c9f155e5e0d4b4a32a8d4fe3469d58cffbc75
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-15 02:21:33 +0900

    [Feat] DetailWeatherView UI 구현

--- OpenWeatherApp/OpenWeatherApp/Presentation/DetailWeatherView/DetailWeatherView.swift
+++ OpenWeatherApp/OpenWeatherApp/Presentation/DetailWeatherView/DetailWeatherView.swift
@@ -5,4 +5,45 @@
 //  Created by 이재웅 on 2023/02/14.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class DetailWeatherView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        let layout = UICollectionViewFlowLayout()
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        delegate = self
+        dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        setupViewStyle()
+    }
+    
+    private func setupViewStyle() {
+        backgroundColor = UIColor(red: 166/255, green: 190/255, blue: 222/255, alpha: 1.0)
+        layer.cornerRadius = 8.0
+    }
+}
+
+extension DetailWeatherView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        
+        return cell
+    }
+}
+
+extension DetailWeatherView: UICollectionViewDelegateFlowLayout {
+    
+}
