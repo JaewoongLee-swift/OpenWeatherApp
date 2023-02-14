15adf65200b245682e733a430af8873e57972aaf
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-14 16:28:55 +0900

    [Feat] WeatherService 구현

---
+++ OpenWeatherApp/OpenWeatherApp/Network/WeatherService.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherService.swift
+//  OpenWeatherApp
+//
+//  Created by 이재웅 on 2023/02/14.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+struct WeatherService {
+    static let shared = WeatherService()
+    
+    private init() { }
+    
+    func requestWeather(at coord: Coordinates) -> Observable<WeatherResponse> {
+        let url = "http://api.openweathermap.org/data/2.5/forecast?lat=\(coord.latitude)&lon=\(coord.longitude)&appid=\(OpenWeatherAPI.key)"
+        
+        return Observable.create { observer in
+            let task = AF.request(url, method: .get)
+                .responseDecodable(of: WeatherResponse.self) { response in
+                    switch response.result {
+                    case .success(let response):
+                        observer.onNext(response)
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                    
+                    observer.onCompleted()
+                }
+            
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+}
+
+typealias Coordinates = (latitude: Double, longitude: Double)
