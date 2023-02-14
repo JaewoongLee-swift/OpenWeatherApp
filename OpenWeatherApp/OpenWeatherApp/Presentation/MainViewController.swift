03432c0e91d4c1167df090104487cb2809d805dd
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-14 09:20:18 +0900

    [Project] 프로젝트 폴더분류 및 코드베이스 세팅

---
+++ OpenWeatherApp/OpenWeatherApp/Presentation/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  OpenWeatherApp
+//
+//  Created by 이재웅 on 2023/02/14.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
