a165336e6daefda7fef07ab31164e86aceb16c2d
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-14 22:29:43 +0900

    [Feat] TodayWeatherView UI 구현

--- OpenWeatherApp/OpenWeatherApp/Presentation/TodayWeatherView/TodayWeatherView.swift
+++ OpenWeatherApp/OpenWeatherApp/Presentation/TodayWeatherView/TodayWeatherView.swift
@@ -5,4 +5,91 @@
 //  Created by 이재웅 on 2023/02/14.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class TodayWeatherView: UIView {
+    private lazy var windSpeedLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16.0)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        return view
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+    
+    func configure() {
+        setupLayout()
+        setupViewStyle()
+        
+        windSpeedLabel.text = "돌풍의 풍속은 최대 4m/s입니다."
+    }
+    
+    private func setupLayout() {
+        [
+            windSpeedLabel,
+            separatorView,
+            collectionView
+        ].forEach { addSubview($0) }
+        
+        windSpeedLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(12.0)
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.trailing.equalToSuperview().inset(16.0)
+        }
+        
+        separatorView.snp.makeConstraints {
+            $0.top.equalTo(windSpeedLabel.snp.bottom).offset(12.0)
+            $0.leading.equalToSuperview().inset(8.0)
+            $0.trailing.equalToSuperview().inset(8.0)
+            $0.height.equalTo(1.0)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(separatorView.snp.bottom).offset(12.0)
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.trailing.equalToSuperview().inset(16.0)
+        }
+    }
+    
+    private func setupViewStyle() {
+        backgroundColor = UIColor(red: 76/255, green: 125/255, blue: 188/255, alpha: 0.7)
+        layer.cornerRadius = 8.0
+    }
+}
+
+extension TodayWeatherView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+}
+
+extension TodayWeatherView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 60.0, height: 100.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20.0)
+    }
+}
