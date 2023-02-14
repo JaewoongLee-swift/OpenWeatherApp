6e2eaa08294e70db93fa46844c55a0993378964b
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-14 10:09:30 +0900

    [Project] View 파일생성 및 폴더분류

---
+++ OpenWeatherApp/OpenWeatherApp/Presentation/DetailWeatherView/DetailWeatherView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailWeatherView.swift
+//  OpenWeatherApp
+//
+//  Created by 이재웅 on 2023/02/14.
+//
+
+import Foundation
---
+++ OpenWeatherApp/OpenWeatherApp/Presentation/WeeklyWeatherView/WeeklyWeatherView.swift
@@ -0,0 +1,8 @@
+//
+//  WeeklyWeatherView.swift
+//  OpenWeatherApp
+//
+//  Created by 이재웅 on 2023/02/14.
+//
+
+import Foundation
