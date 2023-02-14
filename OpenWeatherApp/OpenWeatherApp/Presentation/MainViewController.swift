81e275d4b9e74ae20551c30ee3212c9516dd5c3a
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-14 16:52:05 +0900

    [Feat] MainViewController에 SearchController 및 UINavigationController 설정

--- OpenWeatherApp/OpenWeatherApp/Presentation/MainViewController.swift
+++ OpenWeatherApp/OpenWeatherApp/Presentation/MainViewController.swift
@@ -6,9 +6,33 @@
 //
 
 import UIKit
+import RxSwift
 
 class MainViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewControlletStyle()
+        setNavigationBar()
+    }
+}
+
+extension MainViewController: UISearchBarDelegate {
+    
+}
+
+extension MainViewController {
+    private func setNavigationBar() {
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "Search"
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        
+        navigationItem.searchController = searchController
+    }
+    
+    private func setupViewControlletStyle() {
+        view.backgroundColor = UIColor(red: 166/255, green: 190/255, blue: 222/255, alpha: 1.0)
     }
 }
--- OpenWeatherApp/OpenWeatherApp/Supports/SceneDelegate.swift
+++ OpenWeatherApp/OpenWeatherApp/Supports/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        self.window?.rootViewController = MainViewController()
+        self.window?.rootViewController = UINavigationController(rootViewController: MainViewController())
         self.window?.makeKeyAndVisible()
     }
 
