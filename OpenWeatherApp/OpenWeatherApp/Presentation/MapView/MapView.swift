a15c5c1874b0374479e28212bebd2fffbf9db988
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-16 05:01:39 +0900

    [Feat] MapView 내에 MKMapView 구현

--- OpenWeatherApp/OpenWeatherApp/Presentation/MapView/MapView.swift
+++ OpenWeatherApp/OpenWeatherApp/Presentation/MapView/MapView.swift
@@ -7,8 +7,20 @@
 
 import UIKit
 import SnapKit
+import MapKit
 
 final class MapView: UIView {
+    func configure() {
+        setupLayout()
+        setupViewStyle()
+        
+        let defaultCoord: Coordinates = (37.5683, 126.9778)
+        let centerCoord = CLLocationCoordinate2D(latitude: defaultCoord.latitude, longitude: defaultCoord.longitude)
+        mapView.setRegion(MKCoordinateRegion(center: centerCoord, span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)), animated: true)
+        addCustomPin(defaultCoord)
+        rainfallLabel.text = "강수량"
+    }
+    
     private lazy var rainfallLabel: UILabel = {
         let label = UILabel()
         label.textColor = .secondarySystemBackground
@@ -17,21 +29,34 @@ final class MapView: UIView {
         return label
     }()
     
-    func configure() {
-        setupLayout()
-        setupViewStyle()
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.delegate = self
         
-        rainfallLabel.text = "강수량"
+        return mapView
+    }()
+    
+    private func addCustomPin(_ coordinates: Coordinates) {
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+        mapView.addAnnotation(pin)
     }
     
     private func setupLayout() {
         [
-            rainfallLabel
+            rainfallLabel,
+            mapView
         ].forEach { addSubview($0) }
         
         rainfallLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(8.0)
             $0.leading.equalToSuperview().inset(12.0)
+        }
+        
+        mapView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(300.0)
         }
     }
     
@@ -40,4 +65,8 @@ final class MapView: UIView {
         layer.cornerRadius = 8.0
     }
     
+}
+
+extension MapView: MKMapViewDelegate {
+
 }
