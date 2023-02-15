36f5777abd6e2b9fe6efc33b92cc87b90bf16036
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-15 13:53:39 +0900

    [Feat] MainViewModel이 데이터를 Observable로 발행하도록 수정

--- OpenWeatherApp/OpenWeatherApp/Presentation/MainViewModel.swift
+++ OpenWeatherApp/OpenWeatherApp/Presentation/MainViewModel.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import RxSwift
 
 class MainViewModel {
-    var currentWeather = CurrentWeather(cityName: "Seoul", currentTemp: -7, skyCondition: "맑음", minTemp: -12, maxTemp: -11)
-    var todayWeather = TodayWeather(gustSpeed: 4, hourlyWeather: [
+    private var currentWeatherData = CurrentWeather(cityName: "Seoul", currentTemp: -10, skyCondition: "맑음", minTemp: -12, maxTemp: -11)
+    
+    private var todayWeatherData = TodayWeather(gustSpeed: 4, hourlyWeather: [
         HourlyWeather(time: 11, temperature: -11, skyCondition: "맑음"),
         HourlyWeather(time: 12, temperature: -11, skyCondition: "맑음"),
         HourlyWeather(time: 13, temperature: -11, skyCondition: "맑음"),
@@ -20,12 +22,17 @@ class MainViewModel {
         HourlyWeather(time: 18, temperature: -11, skyCondition: "맑음"),
         HourlyWeather(time: 19, temperature: -11, skyCondition: "맑음")
     ])
-    var weeklyWeather = WeeklyWeather(dayWeather: [
+    private var weeklyWeatherData = WeeklyWeather(dayWeather: [
         DayWeather(day: "오늘", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
         DayWeather(day: "목", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
         DayWeather(day: "금", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
         DayWeather(day: "토", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
         DayWeather(day: "일", skyCondition: "맑음", minTemp: -12, maxTemp: -11)
     ])
-    var detailWeather = DetailWeather(humadity: 56, cloudiness: 50, windSpped: 1.97, gustSpeed: 3.39, pressure: 1030)
+    private var detailWeatherData = DetailWeather(humadity: 56, cloudiness: 50, windSpped: 1.97, gustSpeed: 3.39, pressure: 1030)
+    
+    lazy var currentWeather = Observable.just(currentWeatherData)
+    lazy var todayWeather = Observable.just(todayWeatherData)
+    lazy var weeklyWeather = Observable.just(weeklyWeatherData)
+    lazy var detailWeather = Observable.just(detailWeatherData)
 }
