6b5bec53ce9c510b82ae732549f0021f67b39b2e
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-16 00:39:33 +0900

    [Feat] String타입의 날짜 데이터와 현재 날짜의 요일이 같으면 오늘, 아닐경우 해당요일을 나타내는 extension 구현

--- OpenWeatherApp/OpenWeatherApp/Extension/Extension+String.swift
+++ OpenWeatherApp/OpenWeatherApp/Extension/Extension+String.swift
@@ -15,4 +15,34 @@ extension String {
         
         return Int(hourString) ?? 0
     }
+    
+    var dayInKorea: String {
+        //ex: self = 2023-02-15 23:55:55
+        let days = (self.split(separator: " ").first ?? "2023-01-01").split(separator: "-").map { Int($0) }
+        let times = (self.split(separator: " ").last ?? "00:00:00").split(separator: ":").map { Int($0) }
+        let year = days[0]
+        let month = days[1]
+        let day = days[2]
+        let hour = times[0]
+        let minute = times[1]
+        let second = times[2]
+        
+        let dateComponenets = DateComponents(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
+        let calendar = Calendar.current
+        let thisDate = calendar.date(from: dateComponenets)!
+        
+        let nowDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss E"
+        formatter.locale = Locale(identifier: "ko_KR")
+        
+        let nowDay = formatter.string(from: nowDate).split(separator: " ").last!
+        let thisTimeDay = formatter.string(from: thisDate).split(separator: " ").last!
+        
+        if nowDay == thisTimeDay {
+            return "오늘"
+        } else {
+            return String(thisTimeDay)
+        }
+    }
 }
