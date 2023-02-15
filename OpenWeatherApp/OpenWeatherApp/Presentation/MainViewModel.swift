d7b3194783457053300acaa312d6400f487aecd4
JaewoongLee-swift <jaewoong0624@gmail.com>
2023-02-16 01:08:38 +0900

    [Feat] MainViewModel에서 WeatherService를 호출해 observable을 내보내도록 구현

--- OpenWeatherApp/OpenWeatherApp/Presentation/MainViewModel.swift
+++ OpenWeatherApp/OpenWeatherApp/Presentation/MainViewModel.swift
@@ -9,30 +9,29 @@ import Foundation
 import RxSwift
 
 class MainViewModel {
-    private var currentWeatherData = CurrentWeather(cityName: "Seoul", currentTemp: -10, skyCondition: "맑음", minTemp: -12, maxTemp: -11)
+    lazy var currentWeather = PublishSubject<CurrentWeather>()
+    lazy var todayWeather = PublishSubject<TodayWeather>()
+    lazy var weeklyWeather = PublishSubject<WeeklyWeather>()
+    lazy var detailWeather = PublishSubject<DetailWeather>()
     
-    private var todayWeatherData = TodayWeather(gustSpeed: 4, hourlyWeather: [
-        HourlyWeather(time: 11, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 12, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 13, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 14, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 15, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 16, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 17, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 18, temperature: -11, skyCondition: "맑음"),
-        HourlyWeather(time: 19, temperature: -11, skyCondition: "맑음")
-    ])
-    private var weeklyWeatherData = WeeklyWeather(dayWeather: [
-        DayWeather(day: "오늘", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
-        DayWeather(day: "목", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
-        DayWeather(day: "금", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
-        DayWeather(day: "토", skyCondition: "맑음", minTemp: -12, maxTemp: -11),
-        DayWeather(day: "일", skyCondition: "맑음", minTemp: -12, maxTemp: -11)
-    ])
-    private var detailWeatherData = DetailWeather(humadity: 56, cloudiness: 50, windSpped: 1.97, gustSpeed: 3.39, pressure: 1030)
-    
-    lazy var currentWeather = BehaviorSubject<CurrentWeather>(value: currentWeatherData)
-    lazy var todayWeather = BehaviorSubject<TodayWeather>(value: todayWeatherData)
-    lazy var weeklyWeather = BehaviorSubject<WeeklyWeather>(value: weeklyWeatherData)
-    lazy var detailWeather = BehaviorSubject<DetailWeather>(value: detailWeatherData)
+    init(weatherDomain: WeatherService = WeatherService(), cityDomain: CityStore = CityStore()) {
+        let seoulCoord: Coordinates = (37.5683, 126.9778)
+        let weatherObservable = weatherDomain.requestWeather(at: seoulCoord)
+        
+        _ = weatherObservable
+            .map { $0.getCurrentWeather() }
+            .bind(to: currentWeather)
+        
+        _ = weatherObservable
+            .map{ $0.getTodayWeather() }
+            .bind(to: todayWeather)
+        
+        _ = weatherObservable
+            .map{ $0.getWeeklyWeather() }
+            .bind(to: weeklyWeather)
+        
+        _ = weatherObservable
+            .map{ $0.getDeatilWeather() }
+            .bind(to: detailWeather)
+    }
 }
